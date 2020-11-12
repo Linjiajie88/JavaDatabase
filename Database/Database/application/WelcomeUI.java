1d2907a231d22d52a14deb5e3073d9f5c5f53b51
Linjiajie88 <Lin.jiajie88@gmail.com>
2020-11-12 00:10:05 -0600

    added tableView for quert result

---
+++ Database/Database/application/WelcomeUI.java
@@ -0,0 +1,85 @@
+package application;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.HashMap;
+import javafx.scene.chart.*;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class WelcomeUI {
+	private Button host,manager,server;
+	private HashMap<String, Scene> sceneMap;
+	private Stage primaryStage;
+	private query query;
+	public static String type;
+	public WelcomeUI(HashMap<String, Scene> sceneMap, Stage primaryStage) {
+		this.sceneMap = sceneMap;
+		this.primaryStage = primaryStage;
+	}
+	public Scene createWelcomScene() {
+		
+	 
+	    //creat TableUI
+		Table table = new Table(sceneMap, primaryStage);
+    	sceneMap.put("table", table.createLoginScene());
+		//create buttons
+        server = new Button("test");
+        server.setPrefWidth(90);
+        
+        
+        //LoginUI loginUI = new LoginUI(sceneMap, primaryStage);
+    	//sceneMap.put("login", loginUI.createLoginScene());
+        
+        server.setOnAction(e->{
+        	type = "test";
+        	//Alert alert = new Alert(null);
+            //alert.setTitle("Message Here...");
+            //alert.setContentText("test test test !");
+            //alert.showAndWait();
+        	//primaryStage.setScene(sceneMap.get("login"));
+        	//this.query=new query();
+        	//query.test();
+        	primaryStage.setScene(sceneMap.get("table"));
+        	
+        });
+        /*
+        host.setOnAction(e->{
+        	type = "host";
+        	primaryStage.setScene(sceneMap.get("login"));
+        });
+        manager.setOnAction(e->{
+        	type = "manager";
+        	primaryStage.setScene(sceneMap.get("login"));
+        });
+        
+        //image
+        
+        Image pic = new Image("welcome.png");
+        ImageView b = new ImageView(pic);
+        b.setFitHeight(500);
+        b.setFitWidth(500);
+        b.setPreserveRatio(true);
+        b.setImage(pic);
+        */
+        //hboox
+        //HBox postionBox = new HBox(10,host,server,manager);
+        //postionBox.setLayoutX(200);
+        //postionBox.setLayoutY(200);
+
+        //create pane
+        Group startpage = new Group();
+        startpage.getChildren().addAll(server);
+        Scene StartScene = new Scene(startpage,400,400);
+		return StartScene;
+	}
+
+}
